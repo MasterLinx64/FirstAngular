eff715c473f5f076aaf37b3983dac995a4cde729
William Adamson <william@millum.no>
2020-08-12 10:01:11 +0200

    Added changes to how the hero.component works.

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
@@ -8,10 +10,20 @@ import { Hero } from '../hero';
 
 export class HeroComponent implements OnInit {
 
+  heroes = HEROES;
+
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
+
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+
 
   constructor() { }
 
