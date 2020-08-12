71a143b4032568565d0999337c1fda610d767605
William Adamson <william@millum.no>
2020-08-12 13:18:38 +0200

    Added services - but has broken messages

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service'
+import { MessageService } from '../message.service'
+import { from } from 'rxjs';
 
 
 @Component({
@@ -14,23 +17,26 @@ export class HeroComponent implements OnInit {
 
   heroes = HEROES;
   heros: Hero[]
+  selectedHero: Hero;
+
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
-  selectedHero: Hero;
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
-  constructor(private heroService: HeroService) {}
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit() {
     this.getHeroes();
   }
-  getHeroes() : void {
+
+  getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add('Heroes')
   }
 }
