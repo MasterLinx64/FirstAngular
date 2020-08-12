5b113ae63c0f8633ff64f9e79c7b28fbda223c6f
William Adamson <william@millum.no>
2020-08-12 11:02:19 +0200

    Added a hero Service that aims to add real data.

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service'
+
 
 @Component({
   selector: 'app-hero',
@@ -11,23 +13,23 @@ import { HEROES } from '../mock-heroes';
 export class HeroComponent implements OnInit {
 
   heroes = HEROES;
-
+  heros: Hero[]
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
-
   selectedHero: Hero;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-
-
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
-
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
 }
